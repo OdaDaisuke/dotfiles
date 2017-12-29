ca6ef4c11aa69f19fcaa2232df28af3304e3f529
daisukeoda <OdaDaisuke@users.noreply.github.com>
2017-12-29 13:13:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 ln -s -i ~/dotfiles/.gitconfig ~
 ln -s -i ~/dotfiles/vimrc/.vimrc ~
 ln -s -i ~/dotfiles/bashrc/.bashrc ~
 ln -s -i ~/dotfiles/bashrc/.bash_profile ~
+
+sudo ./home_brew_install.sh
 
 VIM_FILES=(molokai.vim)
 for file in ${VIM_FILES[@]}
