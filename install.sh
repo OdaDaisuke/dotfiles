fcada799500b3635cbbc6dabfd9bac4fa1730d76
OdaDaisuke <hinopon25@gmail.com>
2017-12-29 18:58:14 +0900

    remove vim scheme

--- install.sh
+++ install.sh
@@ -2,9 +2,3 @@ ln -s -i ~/dotfiles/.gitconfig ~
 ln -s -i ~/dotfiles/vimrc/.vimrc ~
 ln -s -i ~/dotfiles/bashrc/.bashrc ~
 ln -s -i ~/dotfiles/bashrc/.bash_profile ~
-
-VIM_FILES=(molokai.vim)
-for file in ${VIM_FILES[@]}
-do
-	sudo ln -s -i ~/dotfiles/vimrc/vimscheme/$file /usr/share/vim/vim74/colors/$file
-done
